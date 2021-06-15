f57777f698ecb963ff37dbb7beec3bc6fb9d77c3
Darkzarich <darkzarichv2@gmail.com>
2021-06-15 23:52:13 +0300

    add list all files & navigation in and out of folders

--- renderer/App.vue
+++ renderer/App.vue
@@ -1,29 +1,89 @@
 <template>
   <div>
-    <pre>
-      {{ files }}
-    </pre>
+    <div class="loader" v-if="loading">...Loading</div>
+    <div v-else class="folder-list">
+      <template v-if="(isWindows && isRoot) || !isRoot">
+        <FolderListItem name=".." @click="goOneLevelUp()" />
+      </template>
+
+      <template v-if="path">
+        <FolderListItem
+          v-for="item in folder"
+          :name="item.name"
+          @click="item.folder ? goOneLevelDown(item.name) : null"
+        />
+      </template>
+      <template v-else-if="isWindows">
+        <FolderListItem
+          v-for="drive in drives"
+          :name="drive"
+          @click="goOneLevelDown(drive)"
+        />
+      </template>
+    </div>
   </div>
 </template>
 
 <script>
+import FolderListItem from "./components/FolderListItem";
+
 export default {
   name: "App",
+  components: {
+    FolderListItem,
+  },
   data() {
     return {
-      files: null,
+      isWindows: window.ipc.system === "win32",
+      path: "/",
+      folder: null,
+      drives: [],
+      loading: false,
     };
   },
   created() {
     this.init();
   },
+  computed: {
+    isRoot() {
+      return /^(.+:)?\/$/m.test(this.path);
+    },
+  },
+  watch: {
+    path(newPath) {
+      if (newPath) {
+        this.requestFolder(newPath);
+      }
+    },
+  },
   methods: {
     async init() {
-      this.files = await window.ipc.request("read-folder", {
-        path: "/",
+      this.loading = true;
+      if (this.isWindows) {
+        await this.requestDrives();
+      }
+      await this.requestFolder(this.path);
+      this.loading = false;
+    },
+    async requestFolder(path) {
+      const res = await window.ipc.request("read-folder", {
+        path,
       });
-
-      console.log(this.files);
+      if (res.success) {
+        this.folder = res.data;
+      }
+    },
+    async requestDrives() {
+      const res = await window.ipc.request("get-disks");
+      if (res.success) {
+        this.drives = res.data;
+      }
+    },
+    goOneLevelUp() {
+      this.path = this.path.slice(0, this.path.lastIndexOf("/"));
+    },
+    goOneLevelDown(name) {
+      this.path = this.path + `/${name}`;
     },
   },
 };
---
+++ renderer/components/FolderListItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="folder-list__item">
+    {{ name }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+
+<style>
+</style>
