b5e5d5620d380641a36baa9a68b4f44197f2b8cc
Darkzarich <darkzarichv2@gmail.com>
2021-06-18 04:33:16 +0300

    add base component for icons

---
+++ renderer/components/Base/BaseIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg
+    :style="`width: ${size}px; height: ${size}px`"
+    :viewBox="`0 0 ${size} ${size}`"
+  >
+    <use :xlink:href="`#${name}`"></use>
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: "",
+    },
+    size: {
+      type: Number,
+      default: 24,
+    },
+  },
+};
+</script>
